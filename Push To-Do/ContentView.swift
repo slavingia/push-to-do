251c6a75960443ada03a87169301516c4d43b410
Sahil Lavingia <sahil.lavingia@gmail.com>
2023-04-22 14:09:26 -0700

    Initial Commit

---
+++ Push To-Do/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Push To-Do
+//
+//  Created by Sahil Lavingia on 4/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Push To-Do/Push_To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Push_To_DoApp.swift
+//  Push To-Do
+//
+//  Created by Sahil Lavingia on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Push_To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
